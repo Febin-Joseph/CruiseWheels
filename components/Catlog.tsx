edf2e2248b5a4e061158fbf10b91b14a2d59aa99
Febin-Joseph <febinachu123@gmail.com>
2023-11-14 16:51:56 +0530

    contributed to catlog section

--- components/Catlog.tsx
+++ components/Catlog.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Catlog = () => {
   return (
-    <section className='border-2 border-red-500 max-container padding-container py-5 pb-20'>
-      <h2 className='bold-32 md:w-full lg:w-full'>Garage</h2>
-      <p className='regular-16 text-gray-30 mt-1'>
-        Explore Our Garage For Your
-        <span className='text-red-500 bold-20 hover:text-black'> Ride...</span>
-      </p>
+    <section className='max-container padding-container py-5 pb-52'>
+        <h2 className='bold-32 md:w-full lg:w-full'>Garage</h2>
+        <p className='regular-16 text-gray-30 mt-1'>
+          Explore Our Garage For Your
+          <span className='text-red-500 bold-20 hover:text-black'> Ride...</span>
+        </p>
 
       <div className='flex flex-row flexEnd xs:gap-1 md:gap-3 lg:gap-5 -mt-20 md:-mt-20'>
         <div className="dropdown dropdown-end">
@@ -23,6 +23,8 @@ const Catlog = () => {
           <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
             <li><a>Benz</a></li>
             <li><a>Porsche</a></li>
+            <li><a>Jaguar</a></li>
+            <li><a>Bentley</a></li>
           </ul>
         </div>
       </div>
