1cd397c1eb0be5e6539caab145a19af9dffc3eab
Febin-Joseph <febinachu123@gmail.com>
2023-11-17 17:32:45 +0530

    user data saving modal completed

--- components/Button.tsx
+++ components/Button.tsx
@@ -6,13 +6,16 @@ type ButtonProps = {
     icon?: string;
     variant: string;
     full?: boolean;
+    onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
+const Button = ({ type, title, icon, variant, full, onClick }: ButtonProps) => {
     return (
         <button
             className={`flexCenter gap-3 rounded-full border ${variant}
-            ${full && 'w-full'}`}>
+            ${full && 'w-full'}`}
+            onClick={onClick}
+            >
             {icon && <Image src={icon} alt={title} width={24} height={24} />}
             <label htmlFor="" className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
         </button>
--- components/Card.tsx
+++ components/Card.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import { MdAirlineSeatReclineExtra } from 'react-icons/md';
 import { PiSteeringWheelFill } from 'react-icons/pi';
@@ -6,11 +6,26 @@ import { FaGasPump } from 'react-icons/fa';
 import Button from './Button';
 
 const Card = (props: any) => {
+    const [isModalOpen, setIsModalOpen] = useState<Boolean>(true);
     const { name, price, image, seat, carType, carAvg } = props.car;
+
+    const handleCardClick = () => {
+        (document as any).getElementById('my_modal_4').showModal();
+        setIsModalOpen(true);
+    };
+
+    const handleRentNowClick = () => {
+        (document as any).getElementById('my_modal_4').showModal();
+        setIsModalOpen(true);
+    }
 
     return (
         <div className="w-[280px] min-h-[350px] mx-auto md:w-[320px] lg:w-full lg:flex mt-10 relative">
-            <div className="bg-white lg:min-w-[280px] rounded-lg overflow-hidden border-gray-10 shadow-lg relative z-10 group hover:border-2 hover:border-gray-10">
+            <div
+                className="bg-white lg:min-w-[280px] rounded-lg overflow-hidden border-gray-10 shadow-lg
+                            relative z-10 group hover:border-2 hover:border-gray-10"
+                onClick={handleCardClick}
+            >
                 <Image
                     src={image[0]?.url}
                     alt="cars"
@@ -48,11 +63,28 @@ const Card = (props: any) => {
                                 type='button'
                                 title='Rent Now'
                                 variant='bg-gradient-to-r from-red-400 via-red-500 to-red-800 text-white h-[50px] rounded-[3px] md:rounded-[10px] w-[200px]'
+                                onClick={handleRentNowClick}
                             />
                         </div>
                     </div>
                 </div>
+
             </div>
+
+            {isModalOpen && (
+                <dialog id="my_modal_4" className="modal">
+                    <div className="modal-box w-11/12 max-w-5xl">
+                        <h3 className="font-bold text-lg">Hello!</h3>
+                        <p className="py-4">Click the button below to close</p>
+                        <div className="modal-action">
+                            <form method="dialog">
+                                {/* if there is a button, it will close the modal */}
+                                <button className="btn">Close</button>
+                            </form>
+                        </div>
+                    </div>
+                </dialog>
+            )}
         </div>
     );
 };
